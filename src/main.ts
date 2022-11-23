91da81cb15e76c12cba0a7ffb0fcc10c7ad33afa
Rasmus Nylander <43042651+RasmusNylander@users.noreply.github.com>
2022-11-23 14:13:01 +0100

    main: Init program and bind texture for height map

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import {setupWebGL} from "./utils/WebGLUtils";
+import {initShadersFromString} from "./utils/initShaders";
+import {mainShader} from "./glsl/shader.src";
 
 function onFatalError(error: Error): void {
 	console.log(error);
@@ -12,7 +14,7 @@ function onFatalError(error: Error): void {
 	return;
 }
 
-export function main(): void { // : Result<void>
+export async function main(): Promise<void> {
 	const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 	if (!canvas) return onFatalError(new Error("Could not find canvas element"));
 	const gl = setupWebGL(canvas);
@@ -21,6 +23,14 @@ export function main(): void { // : Result<void>
 	gl.viewport(0, 0, canvas.width, canvas.height);
 	gl.clearColor(0, 1, 1, 1);
 	gl.clear(gl.COLOR_BUFFER_BIT)
+
+	const program = initShadersFromString(gl, mainShader.vertex, mainShader.fragment);
+	if (!program.ok) return onFatalError(program.error);
+	gl.useProgram(program.value);
+	const heightMap = gl.createTexture();
+	gl.bindTexture(gl.TEXTURE_2D, heightMap);
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 200, 200, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([255, 0, 0, 255]));
+
 
 	return;
 }
