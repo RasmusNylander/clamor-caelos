56503b390167976b4f3301daeb8c75f7739afd41
Rasmus Nylander <43042651+RasmusNylander@users.noreply.github.com>
2022-11-23 11:37:06 +0100

    main: Move main into main.ts

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,3 @@
-import { util } from "webpack";
-import {setupWebGL} from "./utils/WebGLUtils";
+import {main} from "./main";
 
 window.onload = main;
-
-function onFatalError(error: Error): void {
-	console.log(error);
-	let alertMessage: string = error.message;
-	while (error.cause) {
-		error = error.cause as Error;
-		alertMessage += "\n" + error.message;
-	}
-	alert(alertMessage);
-	console.log("Fatal error: " + alertMessage);
-	return;
-}
-
-function main(): void { // : Result<void>
-	const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-	if (!canvas) return onFatalError(new Error("Could not find canvas element"));
-	const gl = setupWebGL(canvas);
-	if (!gl) return onFatalError(new Error("WebGL isn't available"));
-
-	
-
-	gl.clearColor(0, 1, 1, 1);
-	gl.clear(gl.COLOR_BUFFER_BIT);
-	return;
-}
-
-
-
---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import {setupWebGL} from "./utils/WebGLUtils";
+
+function onFatalError(error: Error): void {
+	console.log(error);
+	let alertMessage: string = error.message;
+	while (error.cause) {
+		error = error.cause as Error;
+		alertMessage += "\n" + error.message;
+	}
+	alert(alertMessage);
+	console.log("Fatal error: " + alertMessage);
+	return;
+}
+
+export function main(): void { // : Result<void>
+	const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+	if (!canvas) return onFatalError(new Error("Could not find canvas element"));
+	const gl = setupWebGL(canvas);
+	if (!gl) return onFatalError(new Error("WebGL isn't available"));
+
+
+
+	gl.clearColor(0, 1, 1, 1);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+	return;
+}
