432eeeb9c5f37898f7df0a9dcf589171ae292934
Rasmus Nylander <43042651+RasmusNylander@users.noreply.github.com>
2022-11-23 11:12:32 +0100

    index: Move main into named function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
-window.onload = () => {
+import {setupWebGL} from "./utils/WebGLUtils";
+
+window.onload = main;
+
+function onFatalError(error: Error): void {
+	console.log(error);
+	let alertMessage: string = error.message;
+	while (error.cause) {
+		error = error.cause as Error;
+		alertMessage += "\n" + error.message;
+	}
+	alert(alertMessage);
+	console.log("Fatal error: " + alertMessage);
+	return;
+}
+
+function main(): void { // : Result<void>
 	const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-	console.log(canvas);
-	const gl = canvas.getContext('webgl') as WebGLRenderingContext;
-	gl.clearColor(0, 1, 0, 1);
+	if (!canvas) return onFatalError(new Error("Could not find canvas element"));
+	const gl = setupWebGL(canvas);
+	if (!gl) return onFatalError(new Error("WebGL isn't available"));
+
+	gl.clearColor(0, 1, 1, 1);
 	gl.clear(gl.COLOR_BUFFER_BIT);
+	return;
 }
