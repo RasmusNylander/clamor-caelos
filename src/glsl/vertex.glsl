be9abc2d67e3511bf515995c2fd6d821dfd49575
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2022-11-24 13:59:18 +0100

    Shader: heightmap offset on plane

--- src/glsl/vertex.glsl
+++ src/glsl/vertex.glsl
@@ -1,7 +1,24 @@
 precision mediump float;
 
-uniform sampler2D uSampler;
+attribute vec3 aVertex;
+attribute vec3 aNormal;
+attribute vec3 aHeightmap;
+
+uniform mat4 uProjectionMatrix;
+uniform mat4 uModelViewMatrix;
+//uniform mat4 uNormalMatrix;
+
+uniform sampler2D uHeightMap;
+ 
+mat4 hi;
 
 void main() {
-    gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+    
+    
+    vec4 height = texture2D(uHeightMap, aHeightmap.xy);
+    vec3 pos = aVertex + aNormal * height.r * aHeightmap.z;
+
+    gl_Position = uProjectionMatrix * uModelViewMatrix * vec4(pos, 1.0);
+    
+
 }
