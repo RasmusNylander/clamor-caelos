aac2b1be63cb0602a031b3f669cdd4b2e3cfd992
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2022-11-24 12:24:58 +0100

    Texture: start work on a Texture class
    Supposed to make it easier to work with but not sure if it will be used.

---
+++ src/Texture.ts
@@ -0,0 +1,64 @@
+// Written by Tobias Maneschijn 2022
+
+export interface TextureType {
+  imagePath: string;
+  id: number;
+  width: number;
+  height: number;
+}
+
+/**
+ * Work in progress class for handling textures in a more Object Oriented way
+ */
+
+export class Texture {
+  public imagePath: string;
+
+  public id: number;
+
+  public width: number;
+
+  public height: number;
+
+  public static loadTexture(
+    gl: WebGLRenderingContext,
+    imagePath: string
+  ): Promise<Texture> {
+    return new Promise((resolve, reject) => {
+      const image = new Image();
+      image.onload = () => this.loadTextureFromImage(gl, image).then(resolve);
+      image.src = imagePath;
+    });
+  }
+
+  public static loadTextureFromImage(
+    gl: WebGLRenderingContext,
+    image: HTMLImageElement
+  ): Promise<Texture> {
+    return new Promise((resolve, reject) => {
+      const texture = gl.createTexture();
+      if (!texture) {
+        reject(new Error("Could not create texture"));
+        return;
+      }
+      gl.bindTexture(gl.TEXTURE_2D, texture);
+      gl.texImage2D(
+        gl.TEXTURE_2D,
+        0,
+        gl.RGBA,
+        gl.RGBA,
+        gl.UNSIGNED_BYTE,
+        image
+      );
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    });
+  }
+
+  // Don't use this. Use the static load functions instead.
+  constructor(parameters: TextureType) {
+    this.imagePath = parameters.imagePath;
+    this.id = parameters.id;
+    this.width = parameters.width;
+    this.height = parameters.height;
+  }
+}
