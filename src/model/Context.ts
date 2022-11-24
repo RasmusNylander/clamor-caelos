edc590b373fbd8673313ccc48a6911ce573259b8
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2022-11-24 12:25:26 +0100

    Context: Implement application state context

---
+++ src/model/Context.ts
@@ -0,0 +1,169 @@
+import { identity, Mat4 } from "../utils/MVU";
+
+
+// The interface for the application state context
+export interface Context {
+    /**
+     * The WebGL context
+     */
+    gl: WebGLRenderingContext;
+
+    /**
+     * The WebGLProgram of the main shader
+     */
+    program: WebGLProgram;
+
+    /**
+     * The canvas that the WebGL context is attached to
+     */
+    canvas: HTMLCanvasElement;
+
+    /**
+     * The heightmap in the form of a  WebGL texture
+     */
+    heightMap: WebGLTexture;
+
+    /**
+     * The html element that contains the height map image
+     */
+    heightMapImage: HTMLImageElement;
+
+    // The buffers to send stuff to the GPU
+    buffers: {
+
+        /**
+         * the vertex buffer
+         * This is where the vertices are stored
+         */
+        vertex: WebGLBuffer;
+
+        /**
+         * The normal buffer
+         * This is where the normals are stored to calculate lighting and more
+         */
+        normal: WebGLBuffer;
+
+        /**
+         * The texture coordinate buffer
+         * This is where the texture coordinates are stored to map the texture to the vertices
+         */
+        texture: WebGLBuffer;
+        
+        /**
+         * The index buffer
+         * This is where the indices are stored to tell the GPU which vertices to draw
+         */
+        index: WebGLBuffer;
+    };
+
+    /**
+     * The locations of attributes in the shader program
+     */
+    attributeLocations: {
+
+        /**
+         * The location of the vertex attribute in the shader program
+         */
+        vertex: number;
+
+        /**
+         * The location of the normals attribute in the shader program
+         */
+        normal: number;
+        /**
+         * The location of the heightmap texture attribute in the shader program
+         */
+        heightmap: number;
+    };
+
+    /**
+     * The uniform locations for the shader
+     */
+    uniformLocations: {
+
+        /**
+         * The projection matrix uniform location for the shader
+         */
+        projection: WebGLUniformLocation;
+
+        /**
+         * The view matrix uniform location for the shader
+         */
+        modelView: WebGLUniformLocation;
+
+        /**
+         * The height map uniform location for the shader
+         */
+        normalMatrix: WebGLUniformLocation;
+
+        /**
+         * The height map uniform location for the shader
+         */
+        heightMap: WebGLUniformLocation;
+    }
+
+    /**
+     * The projection matrix for the camera
+     */
+    projectionMatrix: Mat4;
+
+    /**
+     * The model view matrix for the camera
+     *  
+     * This is the matrix that will be used to transform the vertices
+     * of the objects in the scene.
+     */
+    modelViewMatrix: Mat4;
+
+    /**
+     * The normal matrix for the camera. This is the matrix that will be used to transform the normals of the objects in the scene.
+     */
+    normalMatrix: Mat4;
+    
+};
+
+
+const createContext = (gl: WebGLRenderingContext, canvas: HTMLCanvasElement, program: WebGLProgram, heightMap: WebGLTexture, heightMapImage: HTMLImageElement): Context => {
+    
+    const vertex = gl.createBuffer();
+    const normal = gl.createBuffer();
+    const texture = gl.createBuffer();
+    const index = gl.createBuffer();
+
+    if( !vertex || !normal || !texture || !index ) throw new Error("Could not create buffers");
+    
+    const projectionLocation = gl.getUniformLocation(program, "projection");
+    const modelViewLocation = gl.getUniformLocation(program, "modelView");
+    const normalMatrixLocation = gl.getUniformLocation(program, "normalMatrix");
+    const heightMapLocation = gl.getUniformLocation(program, "heightMap");
+
+    if( !projectionLocation || !modelViewLocation || !normalMatrixLocation || !heightMapLocation ) throw new Error("Could not get uniform locations");
+
+    return {
+        gl,
+        canvas,
+        program,
+        heightMap,
+        heightMapImage,
+        buffers: {
+            vertex: vertex,
+            normal: normal,
+            texture: texture,
+            index: index
+        },
+        attributeLocations: {
+            vertex: gl.getAttribLocation(program, "vertex"),
+            normal: gl.getAttribLocation(program, "normal"),
+            heightmap: gl.getAttribLocation(program, "heightmap")
+        },
+        uniformLocations: {
+            projection: projectionLocation,
+            modelView: modelViewLocation,
+            normalMatrix: normalMatrixLocation,
+            heightMap: heightMapLocation
+        },
+        projectionMatrix: identity(4),
+        modelViewMatrix: identity(4),
+        normalMatrix: identity(4)
+    };
+}
