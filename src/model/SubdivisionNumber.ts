24c86bb2c553da15c67afcfd6193ce3a034c46d5
Rasmus Nylander <43042651+RasmusNylander@users.noreply.github.com>
2022-11-25 17:16:01 +0100

    models: Make SubdivisionNumber a class instead of interface

--- src/model/SubdivisionNumber.ts
+++ src/model/SubdivisionNumber.ts
@@ -1,10 +1,15 @@
 import {error, ok, Result} from "../utils/Resulta";
 
-export interface SubdivisionNumber extends Number {}
-export function subdivision(int: number): Result<SubdivisionNumber> {
-	if (isNaN(int)) return error(`Subdivision must be an integer\nGot: ${int}`);
-	if (!isFinite(int)) return error(`Subdivision must be an integer\nGot: ${int}`);
-	if (int % 1 !== 0) return error(`Subdivision must be an integer\nGot: ${int}`);
-	if (int < 1) return error(`Subdivision must be greater than 0\nGot: ${int}`);
-	return ok(int as SubdivisionNumber);
+export class SubdivisionNumber extends Number {
+	private constructor(number: number) {
+		super(number);
+	}
+	static fromNumber(number: number): Result<SubdivisionNumber> {
+		if (isNaN(number)) return error(`Subdivision must be an integer\nGot: ${number}`);
+		if (!isFinite(number)) return error(`Subdivision must be an integer\nGot: ${number}`);
+		if (number % 1 !== 0) return error(`Subdivision must be an integer\nGot: ${number}`);
+		if (number < 1) return error(`Subdivision must be greater than 0\nGot: ${number}`);
+		return ok(new SubdivisionNumber(number));
+	}
+
 }
--- test/SubdivisionNumberTest.ts
+++ test/SubdivisionNumberTest.ts
@@ -1,32 +1,32 @@
-import {subdivision} from "../src/model/SubdivisionNumber";
+import {SubdivisionNumber} from "../src/model/SubdivisionNumber";
 import {expect, expectFailure} from "./utls/Expect";
 
 describe("SubdivisionNumber", () => {
 	it("should return error when given NaN", () => {
-		expectFailure(subdivision(NaN));
+		expectFailure(SubdivisionNumber.fromNumber(NaN));
 	});
 	it("should return error when given Infinity", () => {
-		expectFailure(subdivision(Infinity));
+		expectFailure(SubdivisionNumber.fromNumber(Infinity));
 	});
 	it("should return error when given -Infinity", () => {
-		expectFailure(subdivision(-Infinity));
+		expectFailure(SubdivisionNumber.fromNumber(-Infinity));
 	});
 	it("should return error when given 0", () => {
-		expectFailure(subdivision(0));
+		expectFailure(SubdivisionNumber.fromNumber(0));
 	});
 	it("should succeed when given 1", () => {
-		expect(subdivision(1).ok).toBeEqual(true);
+		expect(SubdivisionNumber.fromNumber(1).ok).toBeEqual(true);
 	});
 	it("should be equal to 2 when given 2", () => {
-		const result = subdivision(2);
+		const result = SubdivisionNumber.fromNumber(2);
 		if (!result.ok) throw new Error("Unexpected failure");
 		expect(result.value).toBeEqual(2);
 	});
 	it("should return error when given 1.1", () => {
-		expectFailure(subdivision(1.1));
+		expectFailure(SubdivisionNumber.fromNumber(1.1));
 	});
 	it("should 1 when given 1.0", () => {
-		const result = subdivision(1.0);
+		const result = SubdivisionNumber.fromNumber(1.0);
 		if (!result.ok) throw new Error("Unexpected failure");
 		expect(result.value).toBeEqual(1);
 	});
