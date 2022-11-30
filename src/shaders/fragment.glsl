b5fb46f21e5b349dcd04bc2b581212f92ea6abe1
Rasmus Nylander <43042651+RasmusNylander@users.noreply.github.com>
2022-11-30 10:01:29 +0100

    shaders: Translate fragment.glsl & vertex.glsl to WebGL2

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,6 +1,8 @@
+#version 300 es
 precision mediump float;
 
-varying vec4 v_Color;
+in vec4 v_Color;
+out vec4 fragColor;
 void main() {
-	gl_FragColor = v_Color;
+	fragColor = v_Color;
 }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,8 +1,9 @@
+#version 300 es
 precision mediump float;
 
-attribute vec3 aPosition;
-attribute vec3 aNormal; 
-attribute vec2 aTexCoords;
+in vec3 aPosition;
+in vec3 aNormal;
+in vec2 aTexCoords;
 
 uniform mat4 uModelMatrix;
 uniform mat4 uViewMatrix;
@@ -14,15 +15,15 @@ uniform mat4 uProjectionMatrix;
 
 uniform sampler2D uHeightMap;
  
-varying vec4 v_Color;
+out vec4 v_Color;
 
 
 void main() {
-
     float depth = 8.0;
 
-    vec4 height = texture2D(uHeightMap, aTexCoords);
-    vec3 pos = aPosition + aNormal * height.r * depth;
+    vec4 heightComponents = texture(uHeightMap, aTexCoords);
+    float height = heightComponents.r;
+    vec3 pos = aPosition + aNormal * height * depth;
     gl_Position = uProjectionMatrix * uViewMatrix * uModelMatrix * vec4(pos, 1.0);
-    v_Color = vec4(height.r, height.r, height.r, 1.0);
+    v_Color = vec4(height, height, height, 1.0);
 }
