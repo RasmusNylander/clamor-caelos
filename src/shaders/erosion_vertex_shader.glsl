705ff51c5c77f8ce3c29e69e242e3d3710f09299
Rasmus Nylander <43042651+RasmusNylander@users.noreply.github.com>
2022-11-30 10:02:37 +0100

    shaders: Translate erosion_vertex_shader.glsl to WebGL2

--- src/shaders/erosion_vertex_shader.glsl
+++ src/shaders/erosion_vertex_shader.glsl
@@ -1,9 +1,10 @@
+#version 300 es
 precision mediump float;
 
-attribute vec3 a_normal;
+in vec3 a_normal;
 uniform sampler2D u_heightmap;
 
-varying vec4 v_normal;
+out vec4 v_normal;
 
 void main() {
 	vec4 v_normal = vec4(texture2D(u_heightmap, gl_PointCoord).rgb, 0.0);
