23f89c966647e2a4f8515c6f289e3012016db95d
Rasmus Nylander <43042651+RasmusNylander@users.noreply.github.com>
2022-12-01 16:06:37 +0100

    shaders: Make dissolve_fragment_shader take flow direction map
    This makes dissolve_fragment_shader take a map over the flowdirection at
    every point, thereby enabling it to calculate the arriving and leaving
    water.

--- src/shaders/erosion/dissolve_fragment_shader.glsl
+++ src/shaders/erosion/dissolve_fragment_shader.glsl
@@ -1,25 +1,54 @@
 #version 300 es
 precision highp float;
 
-uniform sampler2D u_heightmap_watermap_solutesmap;
-out vec3 height_water_solutes_unused;
+uniform sampler2D u_heightmap_watermap_solutesmap_flowdirectionmap;
+out vec4 height_water_solutes_unused;
+
+float sampleWater(in vec2 textureIndex) {
+	return texture(u_heightmap_watermap_solutesmap_flowdirectionmap, textureIndex).r;
+}
+float sampleHeight(in vec2 textureIndex) {
+	return texture(u_heightmap_watermap_solutesmap_flowdirectionmap, textureIndex).g;
+}
+float sampleSolutes(in vec2 textureIndex) {
+	return texture(u_heightmap_watermap_solutesmap_flowdirectionmap, textureIndex).b;
+}
+float sampleFlowDirection(in vec2 textureIndex) {
+	return texture(u_heightmap_watermap_solutesmap_flowdirectionmap, textureIndex).a;
+}
+
+float arriving_water(in vec2 textureIndex) {
+	return
+		sampleFlowDirection(textureIndex + vec2(-1.0, 1.0)) == 8 ? sampleWater(textureIndex + vec2(-1.0, 1.0)) : 0.0 +
+		sampleFlowDirection(textureIndex + vec2(0.0, 1.0)) == 7 ? sampleWater(textureIndex + vec2(0.0, 1.0)) : 0.0 +
+		sampleFlowDirection(textureIndex + vec2(1.0, 1.0)) == 6 ? sampleWater(textureIndex + vec2(1.0, 1.0)) : 0.0 +
+		sampleFlowDirection(textureIndex + vec2(-1.0, 0.0)) == 4 ? sampleWater(textureIndex + vec2(-1.0, 0.0)) : 0.0 +
+		sampleFlowDirection(textureIndex + vec2(1.0, 0.0)) == 3 ? sampleWater(textureIndex + vec2(1.0, 0.0)) : 0.0 +
+		sampleFlowDirection(textureIndex + vec2(-1.0, -1.0)) == 2 ? sampleWater(textureIndex + vec2(-1.0, -1.0)) : 0.0 +
+		sampleFlowDirection(textureIndex + vec2(0.0, -1.0)) == 1 ? sampleWater(textureIndex + vec2(0.0, -1.0)) : 0.0 +
+		sampleFlowDirection(textureIndex + vec2(1.0, -1.0)) == 0 ? sampleWater(textureIndex + vec2(1.0, -1.0)) : 0.0;
+}
+
+float leaving_water(in vec2 textureIndex) {
+	return sampleFlowDirection(textureIndex) != 5 ? sampleWater(textureIndex) : 0.0;
+}
 
 void main() {
-	float textureIndex = gl_FragCoord;
+	vec2 textureIndex = gl_FragCoord.xy / textureSize(u_heightmap_watermap_solutesmap_flowdirectionmap, 0);
 
 	float evaporation_constant = 0.01;
 	float erosion_strength = 0.1;
 
-	float starting_water_level = texture(u_heightmap_watermap_solutesmap, textureIndex).r;
-	water_level = starting_water_level * (1.0 - evaporation_constant);
+	float starting_water_level = sampleWater(textureIndex);
+	water_level = (starting_water_level + arriving_water(textureIndex) - leaving_water(textureIndex)) * (1.0 - evaporation_constant);
 
-	float starting_solutes_level = texture(u_heightmap_watermap_solutesmap, textureIndex).g;
+	float starting_solutes_level = sampleSolutes(textureIndex);
 	float unused_solvant = max(water_level - starting_solutes_level, 0);
 	solutes_level = min(water_level, starting_solutes_level + unused_solvant * erosion_strength);
 
-	float starting_height = texture(u_heightmap_watermap_solutesmap, textureIndex).b;
+	float starting_height = sampleHeight(textureIndex);
 	float height_change = starting_solutes_level - solutes_level;
 	float height = starting_height + height_change;
 
-	height_water_solutes_unused = vec3(height, water_level, solutes_level);
+	height_water_solutes_unused = vec4(height, water_level, solutes_level, 0.0);
 }
